a09b4c8b61e6f7f1bd89b46805c1855a668b2c20
Vasiliy V Alferov <ya-ikmik2012@yandex.ru>
2015-06-15 22:21:18 +0000

    Scanning for PS/2 devices
    - Now system scans PS/2 devices and writes ports where echo (0xEE) was
      answered with echo (0xEE) and also tries to determine a device type.
    
    TODO: file structure

--- src/kernel.c
+++ src/kernel.c
@@ -3,22 +3,81 @@
 #include "include/vga.h"
 #include "include/stdstring.h"
 
+unsigned char response(unsigned short port)
+{
+    char ans;
+    __asm__ __volatile__ ("inb %1, %0" : "=a"(ans) : "dN"(port));
+    return ans;
+}
+
+unsigned char send_command(unsigned short port, unsigned char val)
+{
+    __asm__ __volatile__ ("outb %0, %1" : : "a"(val), "dN"(port));
+    char ans;
+    __asm__ __volatile__ ("inb %1, %0" : "=a"(ans) : "dN"(port));
+    return ans;
+}
+
+// This calls send_command while the response is 0xfe (RESEND)
+unsigned char get_response_for(unsigned short port, unsigned char val)
+{
+    unsigned char res = send_command(port, val);
+    while (res == 0xfe)
+        res = send_command(port, val);
+    return res;
+}
+
+void print_hex_char(unsigned char c)
+{
+    char buf[10];
+    uctoa(c, buf, 16);
+    vga_puts("0x");
+    vga_puts(buf);
+}
+
+void read_port(unsigned short port)
+{
+    unsigned char ans = get_response_for(port, 0xee);
+    if (ans == 0xee)
+    {
+        print_hex_char(port); vga_puts(" responsed "); print_hex_char(ans);
+        vga_puts(" ... ");
+        ans = get_response_for(port, 0xf5);
+        if (ans != 0xfa)
+        {
+            vga_puts("Something got wrong (");
+            print_hex_char(ans);
+            vga_puts(")\n");
+            return;
+        }
+        ans = get_response_for(port, 0xf2);
+        ans = response(port);
+        print_hex_char(ans);
+        vga_puts(" ");
+        if (ans == 0xab)
+        {
+            ans = response(port);
+            print_hex_char(ans); vga_puts(" ");
+            if (ans == 0xab || ans == 0x41)
+            {
+                print_hex_char(response(port));
+            }
+        }
+        vga_puts("\n");
+    }
+}
+
 void kernel_main(void)
 {
     vga_init();
-    char buf[4];
-    uitoa(1000, buf, 10);
-    vga_puts(buf);
-    vga_putchar('\n');
-    uitoa(255, buf, 16);
-    vga_puts(buf);
-    for (size_t i = 0; i <= 17; i++)
+    for (int port = 0; port <= 0x99; port++)
     {
-        itoa(-i, buf, 10);
-        vga_puts(buf);
-        itoa(-i, buf, 16);
-        vga_puts(buf);
-        vga_putchar('\n');
+        read_port(port);
     }
+    for (int port = 0xa1; port <= 0xff; port++)
+    {
+        read_port(port);
+    }
+    for (;;);
     return;
 }
